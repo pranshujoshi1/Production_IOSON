a54853e4c034ee9f9212cca2ad8ea292f4367361
Pranshu Joshi <joshipranshu111@gmail.com>
2026-05-15 16:16:05 +0530

    added IOSON logo! and some changes done!

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,15 +4,3 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function formatDate(date: string | Date): string {
-  return new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  }).format(new Date(date));
-}
-
-export function truncate(str: string, length: number): string {
-  return str.length > length ? `${str.slice(0, length)}...` : str;
-}
